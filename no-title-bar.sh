b00e632d0ae756a84b47e4501fe4055dc8d1a81b
Murat Çileli <murat.cileli@gmail.com>
2019-04-02 22:53:13 +0300

    Create no-title-bar.sh

---
+++ no-title-bar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+    sleep 2s;
+
+    width="$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/' | cut -f1 -d"x")";
+    id="$(xdotool getwindowfocus)";
+    geometry="$(xdotool getwindowfocus getwindowgeometry | grep -i geometry)";
+
+    if [[ $geometry == *"$width"* ]]; then
+      state="xprop -id $id | grep _MOTIF_WM_HINTS | cut -f2 -d"=")";
+      if [[ $state != " 2, 0, 0, 0, 0" ]]; then
+          xprop -id $id -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
+      fi
+    fi
+done
